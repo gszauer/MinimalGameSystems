1c3f1c21bc09681b8217f323cc538cce84c9e074
gszauer <git@gabormakesgames.com>
2020-08-02 20:02:24 -0700

    Rmoved animation view class

--- Animation/Code/AnimationData.cpp
+++ Animation/Code/AnimationData.cpp
@@ -4,170 +4,6 @@
 
 namespace Animation {
 	int Data::StepLimit = 1000000.0f;
-
-	namespace Internal {
-		class FrameView {
-		protected:
-			float* mBuffer;
-			unsigned int mComponentSize;
-			unsigned int mFrameSize;
-			unsigned int mNumFrames;
-		protected:
-			inline void InterpolateFrames(float* out, float t, float leftTime, const float* valueLeft, const float* outTangentLeft, float rightTime, const float* valueRight, const float* inTangentRight) const {
-				// For linear (and hermite)
-				float frameDelta = rightTime - leftTime;
-
-				// For hermite
-				float tt = t * t;
-				float ttt = tt * t;
-				float h1 = 2.0f * ttt - 3.0f * tt + 1.0f;
-				float h2 = -2.0f * ttt + 3.0f * tt;
-				float h3 = ttt - 2.0f * tt + t;
-				float h4 = ttt - tt;
-
-				for (unsigned int component = 0; component < mComponentSize; ++component) {
-					float valueDelta = valueRight[component] - valueLeft[component];
-					float linearTangent = valueDelta / frameDelta;
-
-					float outTangent = outTangentLeft[component];
-					float inTangent = inTangentRight[component];
-
-					bool stepOut = false;
-					if (outTangent > 0.0f && outTangent >= Animation::Data::StepLimit) {
-						stepOut = true;
-					}
-					if (outTangent < 0.0f && outTangent <= -Animation::Data::StepLimit) {
-						stepOut = true;
-					}
-					
-					bool stepIn = false;
-					if (inTangent > 0.0f && inTangent >= Animation::Data::StepLimit) {
-						stepIn = true;
-					}
-					if (inTangent < 0.0f && inTangent <= -Animation::Data::StepLimit) {
-						stepIn = true;
-					}
-
-					if (stepOut || stepIn) {
-						out[component] = valueLeft[component];
-					}
-					else if (Animation::Internal::FloatCompare(outTangent, linearTangent) && Animation::Internal::FloatCompare(inTangent, linearTangent)) {
-						out[component] = valueLeft[component] + (valueRight[component] - valueLeft[component]) * t;
-					}
-					else { // Cubic
-						float s1 = outTangent * frameDelta;
-						float s2 = inTangent * frameDelta;
-						out[component] = (valueLeft[component] * h1) + (valueRight[component] * h2) + (s1 * h3) + (s2 * h4);
-					}
-				}
-			}
-		private:
-			FrameView();
-			FrameView(const FrameView&);
-			FrameView& operator=(const FrameView&);
-		public:
-			inline FrameView(float* buffer, unsigned int component, unsigned int size) {
-				mBuffer = buffer;
-				mComponentSize = component;
-				mNumFrames = size;
-
-				mFrameSize = 1; // time
-				mFrameSize += mComponentSize; // in
-				mFrameSize += mComponentSize; // value
-				mFrameSize += mComponentSize; // out
-			}
-
-			inline ~FrameView() { }
-
-			inline const float* operator[](unsigned int index) const {
-				return &mBuffer[index * mFrameSize];
-			}
-
-			inline float GetTime(unsigned int index) const {
-				return mBuffer[index * mFrameSize];
-			}
-
-			inline const float* GetIn(unsigned int index) const {
-				return &mBuffer[index * mFrameSize + 1];
-
-			}
-			
-			inline const float* GetValue(unsigned int index) const {
-				return &mBuffer[index * mFrameSize + 1 + mComponentSize];
-			}
-
-			inline const float* GetOut(unsigned int index) const {
-				return &mBuffer[index * mFrameSize + 1 + mComponentSize + mComponentSize];
-			}
-
-			inline unsigned int GetNumComponents() const {
-				return mComponentSize;
-			}
-
-			inline unsigned int GetNumFrames() const {
-				return mNumFrames;
-			}
-
-			inline const float* GetBuffer() const {
-				return mBuffer;
-			}
-
-			inline float GetStartTime() const {
-				return mBuffer[0];
-			}
-
-			inline float GetEndTime() const {
-				return mBuffer[mNumFrames * mFrameSize - mFrameSize];
-			}
-
-			inline void Vec3Interpolate(float* out, unsigned int left, unsigned int right, float t) const {
-				const float* valueLeft = &mBuffer[left * mFrameSize + 1 + mComponentSize];
-				const float* valueRight = &mBuffer[right * mFrameSize + 1 + mComponentSize];
-				const float* outTangentLeft = &mBuffer[left * mFrameSize + 1 + mComponentSize + mComponentSize];
-				const float* inTangentRight = &mBuffer[right * mFrameSize + 1];
-				float leftTime = mBuffer[left * mFrameSize];
-				float rightTime = mBuffer[right * mFrameSize];
-
-				InterpolateFrames(out, t, leftTime, valueLeft, outTangentLeft, rightTime, valueRight, inTangentRight);
-			}
-
-			inline void QuatInterpolate(float* out, unsigned int left, unsigned int right, float t) const {
-				const float* outTangentLeft = &mBuffer[left * mFrameSize + 1 + mComponentSize + mComponentSize];
-				const float* inTangentRight = &mBuffer[right * mFrameSize + 1];
-				float leftTime = mBuffer[left * mFrameSize];
-				float rightTime = mBuffer[right * mFrameSize];
-
-				const float* originalLeft = &mBuffer[left * mFrameSize + 1 + mComponentSize];
-				const float* originalRight = &mBuffer[right * mFrameSize + 1 + mComponentSize];
-
-				float valueLeft[4] = { originalLeft[0], originalLeft[1], originalLeft[2], originalLeft[3] };
-				float valueRight[4] = { originalRight[0], originalRight[1], originalRight[2], originalRight[3] };
-
-				float neighborhood = valueRight[0] * valueLeft[0] + valueRight[1] * valueLeft[1] + valueRight[2] * valueLeft[2] + valueRight[3] * valueLeft[3];
-
-				if (neighborhood < 0.0f) {
-					valueRight[0] = -valueRight[0];
-					valueRight[1] = -valueRight[1];
-					valueRight[2] = -valueRight[2];
-					valueRight[3] = -valueRight[3];
-				}
-
-				InterpolateFrames(out, t, leftTime, valueLeft, outTangentLeft, rightTime, valueRight, inTangentRight);
-
-				// Normalize
-				float lenSq = out[0] * out[0] + out[1] * out[1] + out[2] * out[2] + out[3] * out[3];
-				if (!Animation::Internal::FloatCompare(lenSq, 1.0f)) {
-					if (lenSq > 0.0f) {
-						float invLen = Animation::Internal::InvSqrt(lenSq);
-						out[0] *= invLen;
-						out[1] *= invLen;
-						out[2] *= invLen;
-						out[3] *= invLen;
-					}
-				}
-			}
-		};
-	}
 }
 
 Animation::Data::Data() {
@@ -331,6 +167,7 @@ float Animation::Data::Sample(State& out, float time, bool looping) const {
 }
 
 float Animation::Data::SampleTrack(float* out, unsigned int trackIndex, float time, bool looping) const {
+	float trackTime = time;
 	trackIndex = trackIndex * 4; // 4 = track stride
 
 	unsigned int targetJointId = mTrackData[trackIndex + 0];
@@ -344,110 +181,191 @@ float Animation::Data::SampleTrack(float* out, unsigned int trackIndex, float ti
 	unsigned int offset = mTrackData[trackIndex + 2];
 	unsigned int size = mTrackData[trackIndex + 3];
 	unsigned int numFrames = size / frameStride;
-
-	Animation::Internal::FrameView frameView(&mFrameData[offset], frameSize, numFrames);
-
-	// Find current and next frames
-	float trackStartTime = frameView.GetStartTime();
-	float trackEndTime = frameView.GetEndTime();
-	float trackDuration = trackEndTime - trackStartTime;
-	if (trackDuration <= 0.0f) {
-		return 0.0f;
-	}
-
-	float trackTime = time;
-	if (looping) {
-		trackTime = Animation::Internal::FMod(trackTime - trackStartTime, trackDuration) + trackStartTime;
-		if (trackTime < trackStartTime) {
-			trackTime += trackDuration;
-		}
-	}
-	else {
-		if (trackTime < trackStartTime) {
-			trackTime = trackStartTime;
-		}
-		if (trackTime > trackEndTime) {
-			trackTime = trackEndTime;
-		}
-	}
-
-	// Given a time, find the current and next frame indices
-	unsigned int thisFrame = 0;
-	unsigned int nextFrame = 0;
+	float* frames = &mFrameData[offset];
 	{
-#if 0
-		for (int frameIndex = frameView.GetNumFrames() - 1; frameIndex >= 0; frameIndex -= 1) {
-			if (trackTime >= frameView.GetTime(frameIndex)) {
-				thisFrame = frameIndex;
-				break;
+		// Find current and next frames
+		float trackStartTime = frames[0];
+		float trackEndTime = frames[numFrames * frameStride - frameStride];
+		float trackDuration = trackEndTime - trackStartTime;
+		if (trackDuration <= 0.0f) {
+			return 0.0f;
+		}
+
+		if (looping) {
+			trackTime = Animation::Internal::FMod(trackTime - trackStartTime, trackDuration) + trackStartTime;
+			if (trackTime < trackStartTime) {
+				trackTime += trackDuration;
 			}
 		}
-#else
-		//Since time only ever increases, use a binary search
-		int l = 0;
-		int r = (int)numFrames;
-		while (l <= r) {
-			int m = l + (r - l) / 2;
-			float thisFrameTime = *frameView[m];
-
-			// Found at last frame
-			if (m == numFrames - 1) {
-				if (trackTime >= thisFrameTime) {
-					thisFrame = numFrames - 1;
-					break;
-				}
-			}
-			else {
-				if (trackTime >= thisFrameTime && trackTime <= *frameView[m + 1]) {
-					thisFrame = m;
-					break;
-				}
-
-			}
-
-			// target is greater, ignore left half
-			if (thisFrameTime < trackTime) {
-				l = m + 1;
-			}
-			// target is greater, ignore right half
-			else {
-				r = m - 1;
-			}
-		}
-#endif
-		if (!looping) {
-			if (trackTime <= trackStartTime) {
-				thisFrame = 0;
+		else {
+			if (trackTime < trackStartTime) {
 				trackTime = trackStartTime;
 			}
-			if (trackTime >= trackEndTime) {
-				thisFrame = numFrames - 1;
+			if (trackTime > trackEndTime) {
 				trackTime = trackEndTime;
 			}
 		}
 
-		// Keep nextFrame valid
-		if (thisFrame >= numFrames - 1) {
-			thisFrame = numFrames - 2; // -2 so thisFrame + 1 is a valid index
+		// Given a time, find the current and next frame indices
+		unsigned int thisFrame = 0;
+		unsigned int nextFrame = 0;
+		{
+#if 0
+			for (int frameIndex = frameView.GetNumFrames() - 1; frameIndex >= 0; frameIndex -= 1) {
+				if (trackTime >= frameView.GetTime(frameIndex)) {
+					thisFrame = frameIndex;
+					break;
+				}
+			}
+#else
+			//Since time only ever increases, use a binary search
+			int l = 0;
+			int r = (int)numFrames;
+			while (l <= r) {
+				int m = l + (r - l) / 2;
+				float thisFrameTime = frames[m * frameStride];
+
+				// Found at last frame
+				if (m == numFrames - 1) {
+					if (trackTime >= thisFrameTime) {
+						thisFrame = numFrames - 1;
+						break;
+					}
+				}
+				else {
+					if (trackTime >= thisFrameTime && trackTime <= frames[(m + 1) * frameStride]) {
+						thisFrame = m;
+						break;
+					}
+
+				}
+
+				// target is greater, ignore left half
+				if (thisFrameTime < trackTime) {
+					l = m + 1;
+				}
+				// target is greater, ignore right half
+				else {
+					r = m - 1;
+				}
+			}
+#endif
+			if (!looping) {
+				if (trackTime <= trackStartTime) {
+					thisFrame = 0;
+					trackTime = trackStartTime;
+				}
+				if (trackTime >= trackEndTime) {
+					thisFrame = numFrames - 1;
+					trackTime = trackEndTime;
+				}
+			}
+
+			// Keep nextFrame valid
+			if (thisFrame >= numFrames - 1) {
+				thisFrame = numFrames - 2; // -2 so thisFrame + 1 is a valid index
+			}
+			nextFrame = thisFrame + 1;
 		}
-		nextFrame = thisFrame + 1;
-	}
 
-	// Find frame delta and interpolation t
-	float frameDelta = frameView.GetTime(nextFrame) - frameView.GetTime(thisFrame);
-	float t = 0.0f;
-	if (frameDelta > 0.0f) {
-		t = (trackTime - frameView.GetTime(thisFrame)) / frameDelta;
-	}
+		// Find frame delta and interpolation t
 
-	if (trackComponent == Component::Position) {
-		frameView.Vec3Interpolate(out, thisFrame, nextFrame, t);
-	}
-	else if (trackComponent == Component::Rotation) {
-		frameView.QuatInterpolate(out, thisFrame, nextFrame, t);
-	}
-	else {
-		frameView.Vec3Interpolate(out, thisFrame, nextFrame, t);
+		;
+
+		float frameDelta = frames[nextFrame * frameStride] - frames[thisFrame * frameStride];
+		float t = 0.0f;
+		if (frameDelta > 0.0f) {
+			t = (trackTime - frames[thisFrame * frameStride]) / frameDelta;
+		}
+
+		bool isQuaternion = trackComponent == Component::Rotation;
+		{ // Interpolate
+			const float* originalLeft = &frames[thisFrame * frameStride + 1 + frameSize];
+			const float* originalRight = &frames[nextFrame * frameStride + 1 + frameSize];
+
+			float valueLeft[4] = { originalLeft[0], originalLeft[1], originalLeft[2], originalLeft[3] };
+			float valueRight[4] = { originalRight[0], originalRight[1], originalRight[2], originalRight[3] };
+
+			float leftTime = frames[thisFrame * frameStride];
+			float rightTime = frames[nextFrame * frameStride];
+
+			const float* outTangentLeft = &frames[thisFrame * frameStride + 1 + frameSize + frameSize];
+			const float* inTangentRight = &frames[nextFrame * frameStride + 1];
+
+			if (isQuaternion) {
+				float neighborhood = valueRight[0] * valueLeft[0] + valueRight[1] * valueLeft[1] + valueRight[2] * valueLeft[2] + valueRight[3] * valueLeft[3];
+
+				if (neighborhood < 0.0f) {
+					valueRight[0] = -valueRight[0];
+					valueRight[1] = -valueRight[1];
+					valueRight[2] = -valueRight[2];
+					valueRight[3] = -valueRight[3];
+				}
+			}
+
+			// Interpolate the actual frames
+			{
+				// For linear (and hermite)
+				float frameDelta = rightTime - leftTime;
+
+				// For hermite
+				float tt = t * t;
+				float ttt = tt * t;
+				float h1 = 2.0f * ttt - 3.0f * tt + 1.0f;
+				float h2 = -2.0f * ttt + 3.0f * tt;
+				float h3 = ttt - 2.0f * tt + t;
+				float h4 = ttt - tt;
+
+				for (unsigned int component = 0; component < frameSize; ++component) {
+					float valueDelta = valueRight[component] - valueLeft[component];
+					float linearTangent = valueDelta / frameDelta;
+
+					float outTangent = outTangentLeft[component];
+					float inTangent = inTangentRight[component];
+
+					bool stepOut = false;
+					if (outTangent > 0.0f && outTangent >= Animation::Data::StepLimit) {
+						stepOut = true;
+					}
+					if (outTangent < 0.0f && outTangent <= -Animation::Data::StepLimit) {
+						stepOut = true;
+					}
+
+					bool stepIn = false;
+					if (inTangent > 0.0f && inTangent >= Animation::Data::StepLimit) {
+						stepIn = true;
+					}
+					if (inTangent < 0.0f && inTangent <= -Animation::Data::StepLimit) {
+						stepIn = true;
+					}
+
+					if (stepOut || stepIn) {
+						out[component] = valueLeft[component];
+					}
+					else if (Animation::Internal::FloatCompare(outTangent, linearTangent) && Animation::Internal::FloatCompare(inTangent, linearTangent)) {
+						out[component] = valueLeft[component] + (valueRight[component] - valueLeft[component]) * t;
+					}
+					else { // Cubic
+						float s1 = outTangent * frameDelta;
+						float s2 = inTangent * frameDelta;
+						out[component] = (valueLeft[component] * h1) + (valueRight[component] * h2) + (s1 * h3) + (s2 * h4);
+					}
+				}
+			}
+
+			if (isQuaternion) { // Normalize
+				float lenSq = out[0] * out[0] + out[1] * out[1] + out[2] * out[2] + out[3] * out[3];
+				if (!Animation::Internal::FloatCompare(lenSq, 1.0f)) {
+					if (lenSq > 0.0f) {
+						float invLen = Animation::Internal::InvSqrt(lenSq);
+						out[0] *= invLen;
+						out[1] *= invLen;
+						out[2] *= invLen;
+						out[3] *= invLen;
+					}
+				}
+			}
+		}
 	}
 
 	return trackTime;
