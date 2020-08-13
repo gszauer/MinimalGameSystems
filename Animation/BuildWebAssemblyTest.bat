8930519ac9a1a9a0a8a0b1f57c030ad47e23bb01
gszauer <git@gabormakesgames.com>
2020-08-13 00:57:25 -0700

    Making progress with animations

---
+++ Animation/BuildWebAssemblyTest.bat
@@ -0,0 +1,16 @@
+copy /Y "C:\Users\gszauer\Documents\Git\MinimalGameSystems\Animation\Code\WebAssembly\DownloadTest.js" "C:\Users\gszauer\Documents\Git\MinimalGameSystems\Animation\WebAssembly\animation.js"
+
+REM 8388608 = [8 * 1024 * 1024]
+REM 	-W1,--export-dynamic^
+
+clang^
+	--target=wasm32^
+	-O3^
+	-flto^
+	-nostdlib^
+	-Wl,--no-entry^
+	-Wl,--export-all^
+	-Wl,--allow-undefined-file=Code/WebAssembly/DownloadTest.syms^
+	-Wl,--lto-O3^
+	-Wl,-z,stack-size=8388608^
+	-o WebAssembly/animation.wasm Code/WebAssembly/DownloadTest.c
---
+++ Animation/Code/WebAssembly/DownloadTest.c
@@ -0,0 +1,177 @@
+void StartToLoadFile(const char* address);
+int IsFileLoaded(const char* address);
+int FileSize(const char* address);
+void FileCopyContents(const char* address, char* target);
+void JavascriptLog(const char* message);
+
+char* WriteUInt(char* target, unsigned int v);
+char* WriteFloat(char* target, float v);
+unsigned int CountDigits(unsigned int n);
+
+const char* gFileToDownload = "https://raw.githubusercontent.com/gszauer/MinimalGameSystems/master/Animation/Assets/char_vert.txt";
+int gIsLoaded = 0;
+
+#define export __attribute__((visibility("default")))
+#define import extern
+
+extern unsigned char __heap_base;
+
+unsigned int bump_pointer = (unsigned int) &__heap_base;
+
+void* malloc(unsigned long n) {
+  unsigned int r = bump_pointer;
+  bump_pointer += n;
+  return (void *)r;
+}
+
+void free(void* p) { }
+
+export void DownloadStart() {
+	char printBuffer[512];
+
+	char* print = printBuffer;
+	const char* label = "C: Fetching: ";
+	for (; (*print = *label); ++label, ++print); // StrCpy
+	*print = '\n';
+	print += 1;
+	*print = '\0';
+	JavascriptLog(printBuffer);
+
+	StartToLoadFile(gFileToDownload);
+}
+
+export void DownloadLoop() {
+	char printBuffer[1024];
+	if (gIsLoaded == 0 && IsFileLoaded(gFileToDownload)) {
+		gIsLoaded = 1;
+		
+		unsigned int fileSize = FileSize(gFileToDownload);
+		char* print = printBuffer;
+		const char* label = "C: File is ";
+		for (; (*print = *label); ++label, ++print); // StrCpy
+		print = WriteUInt(print, fileSize);
+		label = "bytes.\n ";
+		for (; (*print = *label); ++label, ++print); // StrCpy
+		*print = '\0';
+		JavascriptLog(printBuffer);
+
+		print = printBuffer;
+		label = "C: File content:\n";
+		for (; (*print = *label); ++label, ++print); // StrCpy
+		FileCopyContents(gFileToDownload, print); // Reads the file contents right into the print buffer
+        print[fileSize] = '\0'; 
+	}
+}
+
+unsigned int CountDigits(unsigned int n) {
+    int length = 0;
+    if (n == 0) {
+        length = 1;
+    }
+    while (n != 0) {
+        n = n / 10;
+        ++length;
+    }
+    return length;
+}
+
+char* WriteFloat(char* target, float v) {
+    if (v < 0) {
+        *target = '-';
+        target += 1;
+        v = -v;
+    }
+
+    unsigned int whole = (unsigned int)v;
+    target = WriteUInt(target, whole);
+
+    // WriteUInt leaves a space at the end of the string
+    // Go back one and replace it with the decimal
+    target -= 1;
+    *target = '.';
+    target += 1;
+
+    float part = v - (float)whole;
+
+    for (unsigned int i = 0; i < 5; ++i) {
+        part = part * 10.0f; // Move up one
+        while (part >= 10.0f) { part -= 10.0f; }
+        whole = (unsigned int)part; // Get Digit
+        //if (whole <= 9) {
+        *target = '0' + whole; // Since it's a single digit
+        target += 1;
+        //}
+    }
+    *target = ' ';
+    target += 1;
+
+    return target;
+}
+
+char* WriteUInt(char* target, unsigned int v) {
+    if (v == 0) {
+        *target = '0';
+        target += 1;
+    }
+    else {
+        unsigned int numDigits = CountDigits(v);
+        char* write = target + numDigits - 1;
+
+        while (v > 10) {
+            unsigned int quotient = v / 10;
+            unsigned int remainder = v % 10;
+
+            *write = '0' + (char)remainder;
+            write -= 1;
+
+            v = quotient;
+        }
+
+        if (v == 10) {
+            *write = '0';
+            write -= 1;
+            *write = '1';
+        }
+        else if (v != 0) {
+            *write = '0' + (char)v;
+        }
+        target += numDigits;
+    }
+
+    *target = ' ';
+    target += 1;
+
+    return target;
+}
+
+import void JS_StartToLoadFile(const char* address, unsigned int stringLen);
+import int JS_IsFileLoaded(const char* address, unsigned int stringLen);
+import int JS_FileSize(const char* address, unsigned int stringLen);
+import void JS_FileCopyContents(const char* address, unsigned int addressLen, char* target);
+import void JS_JavascriptLog(const char* message, unsigned int stringLen);
+
+unsigned int StrLen(const char* str) {
+    const char* s;
+    for (s = str; *s; ++s);
+    return (unsigned int)(s - str);
+}
+
+void StartToLoadFile(const char* address) {
+	JS_StartToLoadFile(address, StrLen(address));
+}
+
+int IsFileLoaded(const char* address) {
+	return JS_IsFileLoaded(address, StrLen(address));
+}
+
+int FileSize(const char* address) {
+	return JS_FileSize(address, StrLen(address));
+}
+
+void FileCopyContents(const char* address, char* target) {
+	JS_FileCopyContents(address, StrLen(address), target);
+}
+
+void JavascriptLog(const char* message) {
+	JS_JavascriptLog(message, StrLen(message));
+}
